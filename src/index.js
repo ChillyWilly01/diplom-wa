d19c51c0d61d6203c67440c734ac1000b406ff91
ChillyWilly01 <32175084+ChillyWilly01@users.noreply.github.com>
2018-06-04 21:32:07 +0300

    Added new files

--- src/index.js
+++ src/index.js
@@ -1,1 +1,25 @@
 import './index.scss';
+import $ from 'jquery';
+
+$(function(){
+    var links = $('.slide__menu, .close_menu');
+    var menu = $('.header-nav');
+
+    links.on('click', function(event) {
+        event.preventDefault();
+        menu.toggleClass('active');
+    });
+
+    menu.on('click', 'a', () => {
+        menu.removeClass('active');
+    })
+});
+
+$(document).ready(function(){
+    $(".header-nav").on("click","a", function (event) {
+        event.preventDefault();
+        var id  = $(this).attr('href'),
+            top = $(id).offset().top;
+        $('body,html').animate({scrollTop: top}, 2000);
+    });
+});
